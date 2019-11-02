d58596ee75083357aaabd52966d9a7442f6be17a
Vincent Lee <vincent.lee@outlook.com>
2019-11-02 11:50:47 -0500

    Add day selection

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-extern crate crypto;
-extern crate pcre;
-extern crate regex;
-extern crate serde_json;
-
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 mod day10;
 mod day11;
 mod day12;
@@ -16,6 +12,26 @@ mod day17;
 mod day23;
 mod util;
 
-fn main() {
-    day17::run();
+fn main() -> Result<(), String> {
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() < 2 {
+	return Err("Please give day".into());
+    }
+    let day = args[1].parse::<usize>().expect("Malformed day number");
+    match day {
+	1 => day1::run(),
+	2 => day2::run(),
+	3 => day3::run(),
+	4 => day4::run(),
+	5 => day5::run(),
+	6 => day6::run(),
+	7 => day7::run(),
+	10 => day10::run(),
+	11 => day11::run(),
+	12 => day12::run(),
+	17 => day17::run(),
+	23 => day23::run(),
+	_ => return Err(format!("Unknown day {}", day)),
+    }
+    return Ok(());
 }
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Read;
+use std::io::BufRead;
 use std::io::BufReader;
 
 pub fn read_all(path: &str) -> Option<String> {
@@ -12,6 +13,17 @@ pub fn read_all(path: &str) -> Option<String> {
         if rdr.read_to_string(&mut input).is_ok() {
             return Some(input);
         }
+    }
+
+    return None;
+}
+
+pub fn lines(path: &str) -> Option<Vec<String>> {
+    let f = File::open(path);
+
+    if f.is_ok() {
+	let rdr = BufReader::new(f.unwrap());
+	return Some(rdr.lines().filter_map(Result::ok).collect());
     }
 
     return None;
