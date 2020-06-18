1f4c4d5a07a8a2e6317d138bed4d46d4fd00336b
Vincent Lee <vincent.lee@outlook.com>
2020-06-18 17:44:52 -0500

    finish d22

--- src/day22.rs
+++ src/day22.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections:: VecDeque;
 
 #[derive(Copy, Clone)]
 enum Spell {
@@ -30,7 +30,7 @@ impl Spell {
 }
 
 /// Stores the total state of the game
-#[derive(Clone, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone)]
 struct State {
     hp: i32,
     mana: i32,
@@ -75,7 +75,14 @@ impl State {
     }
 
     /// Returns mana spent this turn, if any
-    fn tick_player(&mut self, spell: Spell) -> i32 {
+    fn tick_player(&mut self, spell: Spell, p2: bool) -> i32 {
+        if p2 {
+            self.hp -= 1;
+            if self.is_over() {
+                return 0;
+            }
+        }
+
         self.tick_effects();
 
         if self.is_over() {
@@ -119,14 +126,16 @@ impl State {
     }
 
     fn can_cast(&self, spell: Spell) -> bool {
-        // Apply effects that would happen before we cast `spell`
-        let mut tmp = self.clone();
+        // Apply effects that would happen before we cast `spell`, since these affect available mana
+        let mut tmp = *self;
         tmp.tick_effects();
 
+        // Check mana
         if tmp.mana < spell.cost() {
             return false;
         }
 
+        // Check timers
         match spell {
             Spell::Shield => tmp.shield_timer <= 0,
             Spell::Poison => tmp.poison_timer <= 0,
@@ -136,10 +145,10 @@ impl State {
     }
 }
 
-fn step((mana_used, state): (i32, State), spell: Spell) -> (i32, State) {
-    let mut new_state = state.clone();
+fn step((mana_used, state): (i32, State), spell: Spell, p2: bool) -> (i32, State) {
+    let mut new_state = state;
     let mut new_mana_used = mana_used;
-    new_mana_used += new_state.tick_player(spell);
+    new_mana_used += new_state.tick_player(spell, p2);
 
     if !new_state.is_over() {
         new_state.tick_boss();
@@ -148,16 +157,47 @@ fn step((mana_used, state): (i32, State), spell: Spell) -> (i32, State) {
     (new_mana_used, new_state)
 }
 
-pub fn run() {
+fn compute(p2: bool) -> i32 {
     let init = State::new(50, 500, 58, 9);
 
-    /*
-    let mut dists = HashMap::new();
+    let mut best_winning_cost = i32::MAX;
     let mut q = VecDeque::new();
+    q.push_back((0, init));
 
     while !q.is_empty() {
+        let (mana_used, state) = q.pop_front().unwrap();
+
+        // Invariant: the queue should only contain unfinished games
+        assert!(!state.is_over());
+
+        // If our current running mana usage is greater than a best case we know already, stop. We won't be able to do better no matter what.
+        if mana_used >= best_winning_cost {
+            continue;
+        }
+
+        // Try next move
+        for &spell in ALL_SPELLS {
+            if state.can_cast(spell) {
+                let (new_mana_used, new_state) = step((mana_used, state), spell, p2);
+                if new_state.is_over() {
+                    // If player won, record the best mana cost seen so far
+                    if new_state.hp > 0 {
+                        best_winning_cost = best_winning_cost.min(new_mana_used);
+                    }
+                } else {
+                    // Otherwise, keep exploring
+                    q.push_back((new_mana_used, new_state));
+                }
+            }
+        }
     }
-    */
+
+    best_winning_cost
+}
+
+pub fn run() {
+    println!("Part 1: {}", compute(false));
+    println!("Part 1: {}", compute(true));
 }
 
 #[cfg(test)]
@@ -168,7 +208,7 @@ mod test {
         let mut cur = (0, state);
 
         for (i, &spell) in actions.iter().enumerate() {
-            cur = step(cur, spell);
+            cur = step(cur, spell, false);
 
             if i < actions.len() - 1 {
                 assert!(!cur.1.is_over())
